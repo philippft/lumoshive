97662b085919812fa8507b0227257ab0a8f34bfd
philippft <dzosfphilip@gmail.com>
2026-01-28 18:18:45 +0800

    Update Consume API

---
+++ consume-api-react/src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import { Link, NavLink } from "react-router-dom";
+
+export default function Navbar () { 
+  return (
+    <nav className="bg-slate-800 text-white shadow-md">
+      <div className="max-w-7xl mx-auto px-4 py-3 flex justify-between items-center">
+        <Link to="/" className="text-xl font-bold tracking-wide hover:text-slate-300">
+          FakeStore CRUD
+        </Link>
+
+        <div className="flex gap-6">
+          <NavLink to="/" className={({ isActive }) => `hover:text-slate-300 transition ${ isActive ? "font-semibold underline" : "" }`}>
+            Lihat Produk
+          </NavLink>
+
+          <NavLink to="/tambah" className={({ isActive }) => `hover:text-slate-300 transition ${ isActive ? "font-semibold underline" : "" }`}>
+            Tambah Produk
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+
---
+++ consume-api-react/src/routes/AppRoutes.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+// import ProductListPage from "../pages/products/ProductListPage";
+// import AddProductPage from "../pages/products/AddProductPage";
+// import EditProductPage from "../pages/products/EditProductPage";
+
+export default function AppRoutes () {
+    return (
+    <Routes>
+        {/* <Route path="/" element={<ProductListPage />} />
+        <Route path="/products" element={<ProductListPage />} />
+        <Route path="/products/add" element={<AddProductPage />} />
+        <Route path="/products/:id/edit" element={<EditProductPage />} /> */}
+    </Routes>
+    )
+}
