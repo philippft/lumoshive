b91406549e9a41a1e2730c1e02f296ab9cceec97
philippft <dzosfphilip@gmail.com>
2026-02-27 19:21:45 +0800

    add: add layouts

--- mini-proyek-todos-react-advanced-lanjutan/src/App.jsx
+++ mini-proyek-todos-react-advanced-lanjutan/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import TodoListPage from './Page/TodoListPage'
+import Layout from './shared/layouts/Layout'
 
 function App() {
 
   return (
-    <>
+    <Layout>
       <TodoListPage/>
-    </>
+    </Layout>
   )
 }
 
---
+++ mini-proyek-todos-react-advanced-lanjutan/src/shared/layouts/Layout.jsx
@@ -0,0 +1,10 @@
+
+const Layout = ({children}) => {
+    return (
+            <div className="row justify-content-center">
+                <div className="col-md-8 col-lg-6">{children}</div>
+            </div>
+    )
+}
+
+export default Layout
